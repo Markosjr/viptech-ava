f30b38f4c660568c32a18d842cf3bd7e208ca021
markosjr <markosjr1969@gmail.com>
2022-01-27 16:53:40 -0300

    add new pastas and css

--- app/src/App.js
+++ app/src/App.js
@@ -3,6 +3,9 @@ import './App.css'
 import Header from './componentes/Header'
 import Corpo from './componentes/Corpo'
 import Dados from './componentes/Dados'
+import EditarProduto from './EditarProduto'
+import AddProduto from './AddProduto'
+import Carrinho from './Carrinho'
 
 export default function App () {
   return (
--- app/src/componentes/Corpo.js
+++ app/src/componentes/Corpo.js
@@ -8,20 +8,34 @@ export default function Corpo() {
 
     const botaoProduto ={
         backgroundColor: '#0F4C81',
+        float: 'right',
+        width: '291px',
+        height: '64px',
+        right: '261px',
+        top: '164px',
+        borderRadius: '12px',
+        fontFamily: 'Raleway'
+
+
+
+
+        
         
     }
 
     return (
         <section>
 
-        <p style={{fontFamily: 'Raleway', fontSize: '2em'}}> Produtos </p> 
+        <p style={{fontFamily: 'Raleway', fontSize: '2em', padding:'10px', marginLeft:'2em'}}> Produtos </p> 
 
         <button style={botaoProduto}> + Adicionar Produto </button>
 
         <div>
-        <div id="d1"><img src={Cam1}></img></div>
-        <div id='d2'><img src={Cam2}></img></div>
-        <div id='d3'><img src={Cam3}></img></div>
+        <div id="d1"><img src={Cam1}></img>Câmera interna Inteligente Wi-fi Full HD iM4 </div>
+        <br></br>
+        <div id='d2'><img src={Cam2}></img> Câmera interna Inteligente Wi-fi Full HD iM3</div>
+        <br></br>
+        <div id='d3'><img src={Cam3}></img> Cãmera DS-2CD 258G2-I</div>
 
         </div>
         
