226c7c87a6d8d5cde99a855798634902e05998f1
markosjr <markosjr1969@gmail.com>
2022-01-27 17:48:56 -0300

    add forms produto

--- app/src/AddProduto.js
+++ app/src/AddProduto.js
@@ -1,14 +1,42 @@
 import React, { useState } from "react";
+import Header from "./componentes/Header";
 
 export default function AddProduto () {
 
-    const [nome, setNome] = useState('Digite o nome do Produto')
-    return (
-        <>
-        <h1>Adicionar Produto</h1>
 
-        <label>  </label>
-        <input type='text'></input>
-        </>
+    const [nome, setNome] = useState('Digite o nome do Produto')
+    const [marca, setMarca] = useState('Digite a marca do Produto')
+    const [valor, setValor] = useState(' R$ 000.00')
+    const [cor, setCor] = useState('Branco', 'Preto')
+    return (
+        
+        
+        <div>
+        
+            <Header/>
+            <p>Home </p>
+            <p> Adicionar Produto</p>
+            <div className="NomeProduto">
+                Nome do Produto:
+                <input type="text" value={nome} onChange={(event) => setNome(event.target.value) } />
+            </div>
+
+            <div className="MarcaProduto">
+                Marca:
+                <input type="text" value={marca} onChange={(event) => setMarca(event.target.value)} />
+            </div>
+            
+            <div className="ValorProduto">
+                Valor:
+                <input min='0' type="text" value={valor} onChange={(event) => setValor (event.target.value)} />
+            </div>
+
+            <div className="CorProduto">
+                Cor:
+                <input type="search" value={cor} onChange={(event)=> setCor (event.target.value)} />
+            </div>
+
+            {/* <button {''}> Adicionar Produto </button> */}
+        </div>
     );
 }
--- app/src/App.js
+++ app/src/App.js
@@ -18,6 +18,8 @@ export default function App () {
         Alterar='' 
         Excluir=''/>
 
+        {/* <AddProduto/> */}
+
     </>
 
   )
